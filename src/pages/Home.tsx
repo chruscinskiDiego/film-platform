4f65b7c9ff764236a0287553638a6818b111b67f
chruscinskiDiego <121837842+chruscinskiDiego@users.noreply.github.com>
2024-03-24 21:15:38 -0300

    Sucessfull tests in API GET method

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,31 @@
+import { useEffect, useState } from "react"
 import * as Styles from "../styles/pages/Home"
+import api from "../services/api"
+import { ITop10WeeklyMovies } from "../interfaces/Home";
 
 export function Home() {
+
+  const[top10WeeklyMovies, setTop10WeeklyMovies] = useState<ITop10WeeklyMovies[]>([]);
+
+  useEffect( () => {
+
+    api.get('/trending/movie/week').then( (response) => {
+
+      setTop10WeeklyMovies(
+        response.data.results.slice(0 , 10).map((movie:ITop10WeeklyMovies) => movie)
+      )
+
+    }).catch( (err) => {
+
+      console.log(err)
+
+    })
+  }, [])
+
+  useEffect( () => {
+    console.log(top10WeeklyMovies)
+  }, [top10WeeklyMovies])
+
  return (
    <Styles.Container>
      <section id="presentation">
